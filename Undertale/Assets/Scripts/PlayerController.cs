9e92cbbe7d1a5634fd4b605f0cd9c57f20aa13e0
AitorGilGarcia <a25aitgilgar@inspedralbes.cat>
2026-04-14 20:22:37 +0200

    Movimiento personaje

---
+++ Undertale/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    float horizontalSpeed;
+    [SerializeField]
+    float verticalSpeed;
+    //[SerializeField]
+    //float speed; // segunda manerade hacerlo
+    [SerializeField]
+    InputActionAsset actions;
+
+    InputAction up_action;
+    InputAction forward_action;
+
+    Rigidbody2D rb;
+
+    void Start()
+    {
+        actions.Enable();
+        forward_action = actions.FindActionMap("Movement").FindAction("Forward");
+        up_action = actions.FindActionMap("Movement").FindAction("Up");
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+
+    void Update()
+    {
+        //Debug.Log("forwardddd " + forward_action.ReadValue<float>());
+        //Debug.Log("forwardddd " + up_action.ReadValue<float>());
+
+        float foward = forward_action.ReadValue<float>();
+        float up = up_action.ReadValue<float>();
+
+        Vector2 movement_force = new Vector2(foward * horizontalSpeed, up * verticalSpeed);
+        rb.AddForce(movement_force);
+
+
+        // Vector2 moveInput = new Vector2 (foward, up);
+       // rb.MovePosition(rb.position + moveInput * speed * Time.deltaTime);
+
+
+
+    }
+}

3ec0276f1d6b7197adb42c3254c4b8f77ff16f35
unaifortes123 <a25unaforcas@inspedralbes.cat>
2026-04-16 16:14:23 +0200

    Update ConnexioDB.cs
    Correcion de comentarios

--- Undertale/Assets/Scripts/ConnexioDB.cs
+++ Undertale/Assets/Scripts/ConnexioDB.cs
@@ -10,13 +10,17 @@ public class ConnexioDB : MonoBehaviour
 
     void Start()
     {
+        /**
+        * Esta cadena es la cadena de conexión a MongoDB Atlas, tiene mi usuario y contraseña, y el nombre del cluster. 
+        * (Me dijo el david que usaramos un "superusuario" usamos el mio y ya, hay que cambiar todo, ya que hay que añadir la connexion con el programa en java.)
+        */
         string connectionString = "mongodb+srv://a25unaforcas_db_user:PbMYfbacegSK8bNP@cluster0.xbths3k.mongodb.net/";
 
         try
         {
-            client = new MongoClient(connectionString);
-            database = client.GetDatabase("UndertaleDB"); // Your database name
-            usersCollection = database.GetCollection<BsonDocument>("a25unaforcas_db_user"); // Example collection
+            client = new MongoClient(connectionString); // Conexión a MongoDB.
+            database = client.GetDatabase("UndertaleDB"); // Nombre de la base de datos.
+            usersCollection = database.GetCollection<BsonDocument>("a25unaforcas_db_user"); // Datos recogidos del jugador.
         }
         catch (System.Exception e)
         {
